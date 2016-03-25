b1195fa765f743d8f56663b43c8ccfb63e9f3307
Aditya Chatterjee <AdiChat@users.noreply.github.com>
2016-03-25 14:49:31 +0530

    Update BIT.java

--- BIT.java
+++ BIT.java
@@ -1,22 +1,27 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-class BIT{
+class BIT
+{
     static int B[],N;
-    public static void main(String args[])throws IOException{
-        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-        
+    public static void main(String args[])throws IOException
+    {
+    BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
     }
-    public int get(int i){
+    public int get(int i)
+    {
         int res=0;
-        while(i!=0){
+        while(i!=0)
+        {
             res+=B[i];
             i-=(i&-i);
         }
         return res;
     }
-    public void set(int i,int val){
-        while(i<=N){
+    public void set(int i,int val)
+    {
+        while(i<=N)
+        {
             B[i]+=val;
             i+=(i&-i);
         }
