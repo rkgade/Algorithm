677603b6fb9459caf98a22694bba538d36d6a481
Aditya Chatterjee <AdiChat@users.noreply.github.com>
2016-03-31 00:05:46 +0530

    Update Kruskal.java

--- Kruskal.java
+++ Kruskal.java
@@ -1,30 +1,34 @@
-
 */  Author : Aditya Chatterjee
    Minimum Spanning Tree - Kruskal's Algorithm
    Complexity : O(E log E) = O(E log V)
    Using a priority queue
    License : CC0 1.0 Universal
-   */
-   
+*/
 import java.util.*;
-class Edge implements Comparable<Edge>{
+class Edge implements Comparable<Edge>
+{
     int a, b, cost;
-    Edge(int a, int b, int cost){
+    Edge(int a, int b, int cost)
+    {
         this.a = a; this.b = b; this.cost = cost;
     }
     
-    public int compareTo(Edge x){
+    public int compareTo(Edge x)
+    {
         return Integer.compare(this.cost, x.cost);
     }
 }
 
-public class kruskal {
+public class kruskal 
+{
     static int[] ar;
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), m = sc.nextInt();
         PriorityQueue<Edge> pq = new PriorityQueue<Edge>(n+1);
-        while(m-- > 0){
+        while(m-- > 0)
+        {
             int x = sc.nextInt(), y = sc.nextInt(), cost = sc.nextInt();
             pq.add(new Edge(x, y, cost));
         }
@@ -33,11 +37,13 @@ public class kruskal {
         Arrays.fill(ar, -1);
         long sum = 0, nEdges = 0;
         boolean[] vis = new boolean[n+1];
-        while(nEdges != n-1){               //number of edges not equal to v-1 
+        while(nEdges != n-1)
+        {               //number of edges not equal to v-1 
             Edge curr = pq.remove();
             int a = curr.a, b = curr.b;
             int parA = getPar(a), parB = getPar(b);
-            if(parA != parB){
+            if(parA != parB)
+            {
                 sum = sum + curr.cost;
                 union(parA, parB);
                 nEdges++;
@@ -46,13 +52,15 @@ public class kruskal {
         System.out.println(sum);
     }
     
-    static int getPar(int a){
+    static int getPar(int a)
+    {
         while(ar[a] != -1)
             a = ar[a];
         return a;
     }
     
-    static void union(int a, int b){
+    static void union(int a, int b)
+    {
         int parA = getPar(a), parB = getPar(b);
         ar[parB] = parA;
     }
